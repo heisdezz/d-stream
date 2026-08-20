254322a10c5328f2b2f0a28193aa076dcca1b02a
heisdezz <desto4q@gmail.com>
2026-08-20 23:18:53 +0100

    fix(ts): remove side-effect css import from theme.ts and add module declarations

---
+++ src/declarations.d.ts
@@ -0,0 +1,24 @@
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: any;
+  export default value;
+}
